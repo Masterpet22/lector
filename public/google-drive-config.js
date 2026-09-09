8c06dff524a074b9ac9639d8e4a27529a61d2f42
Ezequiel Familia <82124767+Masterpet22@users.noreply.github.com>
2026-09-09 12:56:37 -0400

    Update Google API key in google-drive-config.js

--- public/google-drive-config.js
+++ public/google-drive-config.js
@@ -2,6 +2,6 @@
 // NO pongas aqui client_secret ni tokens OAuth.
 window.NEBULA_GOOGLE_CONFIG = Object.freeze({
   clientId: '516619544212-5ta4uvs0tc3t4t5umb8cgav7ogb6r7bp.apps.googleusercontent.com',
-  apiKey: 'PEGA_AQUI_TU_API_KEY',
+  apiKey: 'AIzaSyCXlFMbLvhc73MryArvRO6Rd7Jp2M5RF9M',
   appId: '516619544212',
 });
